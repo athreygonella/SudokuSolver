45c2cadf4c7ddc484742f3778b9b776002617730
agonella3 <agonella3@gatech.edu>
2020-12-09 20:34:53 -0500

    adds user inputs

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ SudokuSolver
 
 The goal of this project is to create a program that receives a
 partially-filled sudoku puzzle from a Sudoku API (http://www.cs.utep.edu/cheon/ws/sudoku/)
-
 and solves the sudoku.
--- sudokuAPI.py
+++ sudokuAPI.py
@@ -1,20 +1,30 @@
 import json
 from urllib.request import urlopen
 
-size = 4
-level = 2
+def getSudokuData(difficulty):
+    size = 9    # Size of the sudoku
 
-linkAPI = (f'http://www.cs.utep.edu/cheon/ws/sudoku/new/?size={size}&level={level}')
+    requestURL = f'http://www.cs.utep.edu/cheon/ws/sudoku/new/?size={size}&level={difficulty}'
 
-print(linkAPI)
+    with urlopen(requestURL) as response:
+        source = response.read()
 
-with urlopen(linkAPI) as response:
-    source = response.read()
-
-data = json.loads(source)
-
-print()
-print(data)
+    data = json.loads(source)
+    print(data)
 
 # Example of Output
 # {'response': True, 'size': '4', 'squares': [{'x': 0, 'y': 3, 'value': 3}, {'x': 1, 'y': 0, 'value': 3}, {'x': 1, 'y': 2, 'value': 4}, {'x': 2, 'y': 2, 'value': 3}, {'x': 3, 'y': 0, 'value': 4}, {'x': 3, 'y': 1, 'value': 3}]}
+
+if __name__ == '__main__':
+    print('Welcome to SudokuSolver!')
+    difficultyString = input('Enter the desired difficulty of your sudoku puzzle (Easy, Medium, or Hard): ')
+
+    # convert difficultyString to number
+    if difficultyString == 'Easy':
+        difficulty = 1
+    elif difficultyString == 'Medium':
+        difficulty = 2
+    else:
+        difficulty = 3
+
+    getSudokuData(difficulty)
